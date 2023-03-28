2518c2d9af49d5c402f61c09082d9ca0140cf3ac
Mohamed Fardin Khan <62133436+entroky@users.noreply.github.com>
2023-03-28 10:14:43 +0530

    Update streamd.bat

--- streamd.bat
+++ streamd.bat
@@ -1,25 +1,24 @@
 @echo off
-where ffmpeg >nul 2>&1
+rem Check if ffmpeg is installed
+ffmpeg -version > nul 2>&1
 if %errorlevel% == 0 (
     echo ffmpeg is already installed.
 ) else (
-    echo ffmpeg is not installed.
-    rem Check if Chocolatey is installed
-    where choco >nul 2>&1
+    rem Check if choco is installed
+    choco -v > nul 2>&1
     if %errorlevel% == 0 (
-        echo Chocolatey is installed. Installing ffmpeg using Chocolatey...
-        choco install ffmpeg -y --source="'https://community.chocolatey.org/api/v2/'"
-        echo ffmpeg has been installed.
+        echo choco is already installed. Installing ffmpeg using choco...
+        choco install ffmpeg
     ) else (
-        echo Chocolatey is not installed. Installing Chocolatey and ffmpeg...
-        rem Install Chocolatey
-        Set "PS_COMMAND=\"Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex\""
-        powershell -NoProfile -ExecutionPolicy Bypass -Command %PS_COMMAND%
-        echo Chocolatey has been installed. Installing ffmpeg...
-        choco install ffmpeg -y --source="'https://community.chocolatey.org/api/v2/'"
-        echo ffmpeg has been installed.
+        rem Install choco
+        echo choco is not installed. Installing choco...
+        @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+        rem Install ffmpeg using choco
+        echo Installing ffmpeg using choco...
+        choco install ffmpeg
     )
-) 
+)
 :: Prompt the user for the output directory using a popup dialog box
 set "VBS_SCRIPT=%temp%\get_folder.vbs"
 >"%VBS_SCRIPT%" echo set shell = CreateObject("Shell.Application") : set folder = shell.BrowseForFolder(0, "Select Output Directory", 512) : if folder is nothing then wscript.echo "" else wscript.echo folder.self.path
